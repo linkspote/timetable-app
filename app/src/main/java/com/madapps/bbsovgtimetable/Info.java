0b05ab568052904c74d781421a1aa445809b6e3f
webnews2 <kevin_kleiber@web.de>
2017-02-21 13:20:48 +0100

    [Feature] enhanced copyright

--- app/src/main/java/com/madapps/bbsovgtimetable/Info.java
+++ app/src/main/java/com/madapps/bbsovgtimetable/Info.java
@@ -2,8 +2,13 @@ package com.madapps.bbsovgtimetable;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 
 public class Info extends Activity {
@@ -12,6 +17,15 @@ public class Info extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        // get instance of calendar for displaying actual copyright range
+        Calendar cYear = Calendar.getInstance();
+        int iYear = cYear.get(Calendar.YEAR);
+
+        // set text of textView for copyright
+        final TextView tvCopyright = (TextView) findViewById(R.id.textView5);
+        tvCopyright.setText(Html.fromHtml((String) "&copy;").toString() + " 2014 - " + iYear
+                + " MadApps");
 
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setHomeButtonEnabled(true);
