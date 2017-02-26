971d0c0f6ccb5248261140ede48df760733ac023
webnews2 <kevin_kleiber@web.de>
2017-02-26 18:30:02 +0100

    [Feature] Added Fragment for ListView to show day or week data (only example data) [Feature] Added permission request for internet [Feature] Updated README.md [Feature] Updated and added layout files

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# timetable-app
+# Timetable App
 This is the repository for the completely new version of our old timetable app.
+
+# Shortcuts for UI elements, variables and co
+Please be sure to replace "Identifier" with the name that you want to use and that you are using camelCase!
+## UI elements
+* WebView => wv_Identifier
+* ListView => lv_Identifier
+* TextView => tv_Identifier
+
+## Variables
+When you have to use a data type, e.g. when using arrays, make sure you add the prefix of the data type at first like s_arrIdentifier.
+* Int => iIdentifier
+* Float => fIdentifier
+* Double => dIdentifier
+* Char => cIdentifier
+* String => sIdentifier
+* Boolean => bIdentifier
+* Array => arrIdentifier
+* List => lsIdentifier
+* ArrayAdapter => arradIdentifier
+* View => vwIdentifier
+* ListView => lvIdentifier
---
+++ app/src/main/java/com/it_project/fg15a/timetable_app/MainActivityFragment.java
@@ -0,0 +1,48 @@
+package com.it_project.fg15a.timetable_app;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        String [] arrHours = {
+                "Deutsch",
+                "Mathe",
+                "Englisch",
+                "Spanisch",
+                "Sport",
+                "Deutsch",
+                "Mathe",
+                "Englisch",
+                "Spanisch",
+                "Sport"
+        }; // array with example data for ListView fragment
+
+        List<String> lsHours = new ArrayList<>(Arrays.asList(arrHours)); // turn Array into List
+
+        ArrayAdapter<String> arradHours = new ArrayAdapter<>(
+                getActivity(), R.layout.list_item_hours, R.id.tv_Hours, lsHours);
+
+        View vwRoot = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView lvHours = (ListView) vwRoot.findViewById(R.id.lv_Hours); // find ListView in Fragment to fill it with data
+        lvHours.setAdapter(arradHours); // set data of ListView respectively of the ListItems
+
+        return vwRoot;
+    }
+}
