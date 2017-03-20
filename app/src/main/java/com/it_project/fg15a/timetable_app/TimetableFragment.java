fac4b9a58f197f2bceca43be4fd13f10ad8c55b2
webnews2 <kevin_kleiber@web.de>
2017-03-20 18:00:42 +0100

    [Feature] Platform updates [Feature] Implemented prototype for getting web data

--- app/src/main/java/com/it_project/fg15a/timetable_app/TimetableFragment.java
+++ app/src/main/java/com/it_project/fg15a/timetable_app/TimetableFragment.java
@@ -1,12 +1,21 @@
 package com.it_project.fg15a.timetable_app;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,6 +29,35 @@ public class TimetableFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        final View vwRoot = inflater.inflate(R.layout.fragment_timetable, container, false);
+
+        String sUri = "https://bbsovg-magdeburg.de/stundenplan/klassen/09/c/c00042.htm";
+
+        final RequestQueue rqTimetable = Volley.newRequestQueue(this.getContext());
+        StringRequest srTimetablePage = new StringRequest(Request.Method.GET, sUri,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Snackbar.make(vwRoot, "Erfolg!", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                        Toast.makeText(getContext(), response, Toast.LENGTH_LONG).show();
+                        rqTimetable.stop();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Snackbar.make(vwRoot, "Misserfolg!", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                        Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                        rqTimetable.stop();
+                    }
+                }
+        );
+
+        rqTimetable.add(srTimetablePage);
+
         String [] arrHours = {
                 "Deutsch",
                 "Mathe",
@@ -37,9 +75,6 @@ public class TimetableFragment extends Fragment {
 
         ArrayAdapter<String> arradHours = new ArrayAdapter<>(
                 getActivity(), R.layout.list_item_hours, R.id.tv_Hours, lsHours);
-
-        View vwRoot = inflater.inflate(R.layout.fragment_timetable, container, false);
-
 
         ListView lvHours = (ListView) vwRoot.findViewById(R.id.lv_Hours); // find ListView in Fragment to fill it with data
         lvHours.setAdapter(arradHours); // set data of ListView respectively of the ListItems
--- app/src/main/java/com/it_project/fg15a/timetable_app/helpers/dataReceiver.java
+++ app/src/main/java/com/it_project/fg15a/timetable_app/helpers/dataReceiver.java
@@ -3,10 +3,6 @@ package com.it_project.fg15a.timetable_app.helpers;
 import android.content.Context;
 import android.webkit.WebView;
 
-/**
- * Created by Schlepptop on 28.02.2017.
- */
-
 public class dataReceiver {
 
     protected String sClass, sWeek;
@@ -17,16 +13,7 @@ public class dataReceiver {
         this.sClass = p_sClass;
         this.sWeek = p_sWeek;
         this.cContext = p_cContext;
-
     }
 
     // TODO: create function to get timetable for chosen class
-    public WebView wv_arrGetWebData (){
-        // connect to internet
-        // receive data for class
-        WebView wvTimeTable = new WebView(cContext);
-        wvTimeTable.loadUrl("");
-
-        return wvTimeTable;
-    }
 }
